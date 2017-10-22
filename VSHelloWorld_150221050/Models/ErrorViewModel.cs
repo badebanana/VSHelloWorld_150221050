8d92c87018125944bde37f0b94c0af0753e9479e
badebanana <barb.97@hotmail.com>
2017-10-22 09:41:54 +0100

    Adicionar arquivos de projeto.

---
+++ VSHelloWorld_150221050/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSHelloWorld_150221050.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
